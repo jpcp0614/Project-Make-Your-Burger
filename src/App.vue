764728d8033938482165b9a672d6bde3f4ff7152
jpcp0614 <jpcp0614@gmail.com>
2023-08-04 20:48:34 -0300

    :sparkles: feat - add component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <Navbar />
   <router-view/>
+  <Footer />
 </template>
 
 <script>
   import Navbar from './components/Navbar';
+  import Footer from './components/Footer';
 
   export default {
     name: 'App',
     components: {
       Navbar,
+      Footer,
     }
   }
 </script>

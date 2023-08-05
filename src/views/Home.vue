fea90031bf913a729229749b4e3df96ae7a9f7ba
jpcp0614 <jpcp0614@gmail.com>
2023-08-05 00:12:36 -0300

    :sparkles: feat - BurgerForm component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <Banner />
   <div class="main-container">
     <h1>Monte o seu hambúrguer</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
 
   import Banner from '../components/Banner';
+  import BurgerForm from '../components/BurgerForm';
 
   export default {
     name: 'Home',
     components: {
       Banner,
+      BurgerForm,
     }
   }
 </script>

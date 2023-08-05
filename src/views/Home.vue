edbc95a11a6678ee167470742a81e0c1d3486fc6
jpcp0614 <jpcp0614@gmail.com>
2023-08-04 23:07:23 -0300

    :sparkles: feat - home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,18 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte o seu hambúrguer</h1>
   </div>
 </template>
 
 <script>
 
+  import Banner from '../components/Banner';
+
+  export default {
+    name: 'Home',
+    components: {
+      Banner,
+    }
+  }
 </script>

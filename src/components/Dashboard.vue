dc75671c68953231ae9430a5c17b0ed112885cf4
jpcp0614 <jpcp0614@gmail.com>
2023-08-08 20:03:01 -0300

    :sparkles: feat - create dashboard and orders

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="burger-table">
+    <div>
+      <div class="" id="burger-table-heading">
+        <div class="order-id">#:</div>
+        <div>Cliente:</div>
+        <div>Pão:</div>
+        <div>Carne:</div>
+        <div>Opcionais:</div>
+        <div>Ações:</div>
+      </div>
+    </div>
+    <div id="burger-table-rows">
+      <div class="burger-table-row">
+        <div class="order-number">1</div>
+        <div>João</div>
+        <div>Pão de Trigo</div>
+        <div>Maminha</div>
+        <div>
+          <ul>
+            <li>Salame</li>
+            <li>Tomate</li>
+          </ul>
+        </div>
+        <div>
+          <select name="status" class="status">
+            <option value="">Status</option>
+          </select>
+          <button class="delete-btn">Cancelar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+}
+</script>
+
+<style scoped>
+
+  #burger-table {
+    margin: 0 auto;
+    max-width: 1200px;
+  }
+
+  #burger-table-heading,
+  #burger-table-rows,
+  .burger-table-row {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  #burger-table-heading {
+    border-bottom: 3px solid #333;
+    font-weight: bold;
+    padding: 12px;
+  }
+
+  #burger-table-heading div,
+  .burger-table-row div {
+    width: 19%;
+  }
+
+  .burger-table-row {
+    border-bottom: 1px solid #ccc;
+    padding: 12px;
+    width: 100%;
+  }
+
+  #burger-table-heading .order-id,
+  .burger-table-row .order-number {
+    width: 5%;
+  }
+
+  select {
+    margin-right: 12px;
+    padding: 12px 6px;
+  }
+
+  .delete-btn {
+    background-color: #222;
+    border: 2px solid #222;
+    color: #FCBA03;
+    cursor: pointer;
+    font-size: 16px;
+    font-weight: bold;
+    margin: 0 auto;
+    padding: 10px;
+    transition: .5s;
+  }
+
+  .delete-btn:hover {
+    background-color: transparent;
+    color: #222;
+  }
+
+</style>
--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="main-container">
-    <h1>PEDIDOS</h1>
+    <h1>Gerenciar Pedidos:</h1>
+    <Dashboard />
   </div>
 </template>
 
 <script>
+
+  import Dashboard from '../components/Dashboard';
+
   export default {
     name: 'Orders',
+    components: {
+      Dashboard,
+    }
   }
 </script>
+
+<style scoped>
+
+</style>
